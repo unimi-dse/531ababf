bb0194d949cb545f27c32e86e1bad8df98242df7
Luca Cerabone <luca@MacBook-Air-di-Luca.local>
2020-01-27 12:25:33 +0100

    minor update

--- R/hello.R
+++ R/hello.R
@@ -1,30 +1,24 @@
 #' Importing data
 #' @title Importing flights data from the nycflights13 dataset
-#' @description Reading from the csv file and storing the data frame
-#' in the variable named 'flights'
+#' @description Reading from the csv file and storing the data frame in the variable named 'flights'
 #' @export
 catch_data <- function(){
-flights <- read_csv("Downloads/flights.csv")
+flights <- read.csv("https://query.data.world/s/kho6cpswdlcnjancrj53o4e3wwv365", header=TRUE, stringsAsFactors=FALSE)
 }
 #' Plotting American Airlines departure and arrival delay
 #' @title Scatter plot of American Airlines departure and arrival delay
-#' @description Plotting AA departure and arrival delay
-#' to check if there is some sort of dependence, and as we can see
-#' as dep_delays increase, also arr_delays increase.
+#' @description Plotting AA departure and arrival delay to check if there is some sort of dependence, and as we can see as dep_delays increase, also arr_delays increase.
 #' @export
 lemmesee <- function(){
 all_aa_flights <- flights %>%
   filter(carrier == "AA")
-
 ggplot(data = all_aa_flights, mapping = aes(x = dep_delay, y = arr_delay)) +
   geom_point()
 }
 #' Calculating the mean arrival delay for American Airlines in 2013
 #' @title American Airlines mean arrival delay
-#' @description The function calculate the mean arrival delay for
-#' American Airlines in 2013
+#' @description The function calculate the mean arrival delay for American Airlines in 2013
 #' @export
 mean_aa <- function(){
 sprintf("For American Airlines, the mean arrival delay in 2013 was: %f", mean(all_aa_flights$arr_delay, na.rm = T))
 }
-
