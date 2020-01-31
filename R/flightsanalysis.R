0ee65999f4cecf3f242eac8ccd4f0c77b1e4510b
Luca Cerabone <luca@MacBook-Air-di-Luca.local>
2020-01-31 12:37:06 +0100

    update

--- R/flightsanalysis.R
+++ R/flightsanalysis.R
@@ -1,11 +1,15 @@
 #' Flightsanalysis Package
 #'
-#' This is a package for the 'Coding for Data Science and Data Management' course
-#' at UNIMI. It analyzes 2013 New York flights data.
+#' This is a package for the 'Coding for Data Science and Data Management - R module' course
+#' at UNIMI. It contains 3 functions to analyze 2013 New York flights data:
+#' * catch(): to retrieve the dataset
+#' * obs(): to plot the variables arr_delay and dep_delay
+#' * mean_arrdelay(): to calculate the average arrival delay for American Airlines flights departed from NYC in 2013
+#' @md
 #'
 #' @docType package
 #'
-#' @author lucacerab \email{luca.cerabone@studenti.unimi.it}
+#' @author Luca Cerabone \email{luca.cerabone@studenti.unimi.it}
 #'
 #' @name flightanalysis
 NULL
