83fb85f5911d49990ee94078058a7ec358c9e052
Luca Cerabone <luca@MacBook-Air-di-Luca.local>
2020-02-01 18:27:47 +0100

    general fixes

--- R/fcns.R
+++ R/fcns.R
@@ -1,7 +1,7 @@
 #' Import data and filter American Airlines flights
 #'
 #' Data for all flights that departed NYC (JFK, LGA or EWR) in 2013, taken from the nycflights13 package dataset.
-#' You can look at data frames clicking on them in the Global Environment.
+#' You can look at American Airlines data frame by clicking on it in Global Environment.
 #' For our analysis, we're interested in columns:
 #' * 'dep_delay, arr_delay': departure and arrival delays in minutes; negative times represent early departures/arrivals.
 #' * 'carrier': two letter carrier abbreviation.
@@ -9,9 +9,9 @@
 #'
 #' @export
 catch <- function(){
-  flights <<- read.csv("https://query.data.world/s/kho6cpswdlcnjancrj53o4e3wwv365", header = TRUE, stringsAsFactors = FALSE)
+  flights <- read.csv("https://query.data.world/s/kho6cpswdlcnjancrj53o4e3wwv365", header = TRUE, stringsAsFactors = FALSE)
   aa_flights <<- flights[grepl('AA', flights$carrier), ]
-  head(flights)
+  return(flights)
 }
 
 #' American Airlines departure and arrival delays plot
@@ -22,8 +22,8 @@ catch <- function(){
 #'
 #' @export
 obs <- function(){
-  ggplot(data = aa_flights, mapping = aes(x = dep_delay, y = arr_delay)) +
-    labs(title = "American Airlines arr/dep delays") +
+  ggplot(aa_flights, aes(x = dep_delay, y = arr_delay)) +
+    ggtitle("American Airlines arr/dep delays") +
     theme(plot.title = element_text(size = 18, hjust = 0.5)) +
     xlab("departure delay (mins)") +
     ylab("arrival delay (mins)") +
@@ -35,9 +35,8 @@ obs <- function(){
 #' The function calculates the average arrival delay for American Airlines flights departed from New York in 2013.
 #'
 #' @export
-
 mean_arrdelay <- function(){
-  arrdelay <- aa_flights[aa_flights$arr_delay >= 0, ] #negative delays are not delays
+  arrdelay <- aa_flights[aa_flights$arr_delay >= 0, ] # negative time is not delay
   sprintf("For American Airlines flights departed from NYC in 2013, the average arrival delay was: %f minutes", mean(arrdelay$arr_delay , na.rm = T))
 }
 
